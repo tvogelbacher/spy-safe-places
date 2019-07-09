b16927c298c263ad5c7ea40563484a4d737fdb08
tvogelbacher <52699512+tvogelbacher@users.noreply.github.com>
2019-07-09 16:23:52 +0200

    safe_spaces.py
    For the necessary conversion from alphanumerical to 0-indexed coordinates in the first level i decided
    to search for the letter's position in the unicode, since that was faster than using a dictionary as i
    found out using timeit. This is inversed in level 3 using the chr() function
    
    I had two approaches for the second level in my mind. One was to see the agents' locations as origins of a
    spreading area that once it had made contact with another areas border would not expand further into that direction.
    This would ensure that every coordinate's  distance to an agent would only have to be calculated once
    (unless it's a border between areas) and would always be the minimum. 
    The other approach just calculated every coordinate's distance to every agent and compared the minimum found distance
    of that specicific coordinate to the greatest found distance on the entire grid. If it's equal to the greatest,
    the coordinate is added to the safe_spots list. If it's larger than the greatest, the safe_spots list is emptied
    and the greatest distance raised.
    In the end i went with the second approach as i felt it was sufficient for a 10x10 grid and therefore a respectively
    small dataset. With a growing matrix however the first approach might safe more time. Another problem I faced and was
    not able to overcome was to code the "don't move in that direction anymore" without the algorithm checking for every
    coordinate, since that is actually slower than just calculating the distance.

--- python/safe_spaces.py
+++ python/safe_spaces.py
@@ -1,45 +1,45 @@
 """Solve the spy game!"""
-
 class SafetyFinder:
     """A class that contains everything we need to find the
     safest places in the city for Alex to hide out
     """
 
     def convert_coordinates(self, agents):
-        """This method should take a list of alphanumeric coordinates (e.g. 'A6')
-        and return an array of the coordinates converted to arrays with zero-indexing.
-        For instance, 'A6' should become [0, 5]
-
-        Arguments:
-        agents -- a list-like object containing alphanumeric coordinates.
-
-        Returns a list of coordinates in zero-indexed vector form.
-        """
-        pass
+        pos_0index = []
+        for i in agents:
+            if int(i[1:]) < 11:#so coordinates like 'A12' are not further processed(10x10 matrix)
+                #looking for position in unicode is faster than dictionary
+                pos_0index.append([ord(i[0]) - 65, int(i[1:]) - 1])
+        return pos_0index
 
     def find_safe_spaces(self, agents):
-        """This method will take an array with agent locations and find
-        the safest places in the city for Alex to hang out.
-
-        Arguments:
-        agents -- a list-like object containing the map coordinates of agents.
-            Each entry should be formatted in indexed vector form,
-            e.g. [0, 5], [3, 7], etc.
-
-        Returns a list of safe spaces in indexed vector form.
-        """
-        pass
+        #checks every coordinate's distance to every agent, since a subtraction is faster than a comparison to clarify whether to subtract or not
+        #dictionary hash table not that useful, since only the greatest distance is of interest??
+        safe_spots = []
+        greatest_distance = 0
+        x = 0
+        if len(agents) != 0:
+            while(x != 10):
+                y = 0
+                while(y - 10 != 0):
+                    distance = min(abs(i[0] - x) + abs(i[1] - y) for i in agents)
+                    if distance == greatest_distance:#happen more often than finding a greater distance
+                        safe_spots.append([x, y])
+                    elif distance > greatest_distance:
+                        greatest_distance = distance
+                        safe_spots.clear()
+                        safe_spots.append([x, y])
+                    y += 1
+                x += 1
+        return safe_spots
 
     def advice_for_alex(self, agents):
-        """This method will take an array with agent locations and offer advice
-        to Alex for where she should hide out in the city, with special advice for
-        edge cases.
-
-        Arguments:
-        agents -- a list-like object containing the map coordinates of the agents.
-            Each entry should be formatted in alphanumeric form, e.g. A10, E6, etc.
-
-        Returns either a list of alphanumeric map coordinates for Alex to hide in,
-        or a specialized message informing her of edge cases
-        """
-        pass
+        advice = []
+        for i in self.find_safe_spaces(self.convert_coordinates(agents)):
+            advice.append(chr(i[0] + 65) + str(i[1] + 1))
+        if advice == []:
+            return 'The whole city is safe for Alex! :-)'
+        elif len(advice) == 100:
+            return 'There are no safe locations for Alex! :-('
+        else:
+            return advice
