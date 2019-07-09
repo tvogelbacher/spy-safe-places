a26cf2177794b5ea0542cd016fdb81393aed05e4
tvogelbacher <52699512+tvogelbacher@users.noreply.github.com>
2019-07-09 20:36:48 +0200

    My revised approach
    -Put back the comments (Sorry for removing them)
    -Changed the way to check for an empty list in lines 37, 65
    -Took out the redundant i [1:] in level 1 by storing the answer in a variable and calling it twice (noticed the possibility before, but thought it would come out even)
    -Renamed the list that contains the zero indexed coordinates to zero_indexed
    
    I did not change:
    -The constants in lines 18, 38, 40, 67 that are based on the limits of the "city" given in the tests for the solutions as well as the Read.me, namely 10x10. The possibility of a changing size in never mentioned. I don't know how to improve anything here unless i get the limits for example from the user as an argument.
    -The for loop in line 16. The given suggestion on improvement(?) "for (agent : agents)" includes a syntactical error. Also it remains unclear to me what's supposed to be improved.
    -The problem of performance consideration of two nested loops in lines 38, 40 as i don´t know how to merge them into one without putting a check, whether a column is done, in there. This is right now handled by the outer loop reducing the times this check has to be made from 100 to 10. Futhermore the inner loop doesn't mess with the requirement for the outer loop, so a break becomes unnecessary? What is the downside?

--- python/safe_spaces.py
+++ python/safe_spaces.py
@@ -5,25 +5,41 @@ class SafetyFinder:
     """
 
     def convert_coordinates(self, agents):
-        pos_0index = []
+        """This method should take a list of alphanumeric coordinates (e.g. 'A6')
+        and return an array of the coordinates converted to arrays with zero-indexing.
+        For instance, 'A6' should become [0, 5]
+        Arguments:
+        agents -- a list-like object containing alphanumeric coordinates.
+        Returns a list of coordinates in zero-indexed vector form.
+        """
+        zero_indexed = []
         for i in agents:
-            if int(i[1:]) < 11:#so coordinates like 'A12' are not further processed(10x10 matrix)
+            matrix_limit = int(i[1:])
+            if matrix_limit < 11:#so coordinates like 'A12' are not further processed(10x10 matrix)
                 #looking for position in unicode is faster than dictionary
-                pos_0index.append([ord(i[0]) - 65, int(i[1:]) - 1])
-        return pos_0index
+                zero_indexed.append([ord(i[0]) - 65, matrix_limit - 1])
+        return zero_indexed
 
     def find_safe_spaces(self, agents):
+        """This method will take an array with agent locations and find
+        the safest places in the city for Alex to hang out.
+        Arguments:
+        agents -- a list-like object containing the map coordinates of agents.
+            Each entry should be formatted in indexed vector form,
+            e.g. [0, 5], [3, 7], etc.
+        Returns a list of safe spaces in indexed vector form.
+        """
         #checks every coordinate's distance to every agent, since a subtraction is faster than a comparison to clarify whether to subtract or not
         #dictionary hash table not that useful, since only the greatest distance is of interest??
         safe_spots = []
         greatest_distance = 0
         x = 0
-        if len(agents) != 0:
+        if agents:
             while(x != 10):
                 y = 0
                 while(y - 10 != 0):
                     distance = min(abs(i[0] - x) + abs(i[1] - y) for i in agents)
-                    if distance == greatest_distance:#happen more often than finding a greater distance
+                    if distance == greatest_distance:#happens more often than finding a greater distance
                         safe_spots.append([x, y])
                     elif distance > greatest_distance:
                         greatest_distance = distance
@@ -34,10 +50,19 @@ class SafetyFinder:
         return safe_spots
 
     def advice_for_alex(self, agents):
+        """This method will take an array with agent locations and offer advice
+        to Alex for where she should hide out in the city, with special advice for
+        edge cases.
+        Arguments:
+        agents -- a list-like object containing the map coordinates of the agents.
+            Each entry should be formatted in alphanumeric form, e.g. A10, E6, etc.
+        Returns either a list of alphanumeric map coordinates for Alex to hide in,
+        or a specialized message informing her of edge cases
+        """
         advice = []
         for i in self.find_safe_spaces(self.convert_coordinates(agents)):
             advice.append(chr(i[0] + 65) + str(i[1] + 1))
-        if advice == []:
+        if not advice:
             return 'The whole city is safe for Alex! :-)'
         elif len(advice) == 100:
             return 'There are no safe locations for Alex! :-('
